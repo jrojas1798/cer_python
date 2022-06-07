5db80b7be75077d9a77110828ced2fbf849e2bd4
“jrojas1798” <juan.jr1278@gmail.com>
2022-06-07 17:46:54 -0500

    Ejercicios de Lab Modulo 2

---
+++ basico/modulo2/lab21119.py
@@ -0,0 +1,22 @@
+#Laboratorio 2.1.1.19
+print("        *\n       * *\n      *   *")
+print("     *     *\n    *       *\n   *         *")
+print("  *           *\n *             *\n******     ******")
+print("      *   *\n      *   *\n      *   *\n      *   *\n      *****")
+
+
+#Flecha Duplicada
+print("          *                "*2)
+print("         * *               "*2)
+print("        *   *              "*2)
+print("       *     *             "*2)
+print("      *       *            "*2)
+print("     *         *           "*2)
+print("    *           *          "*2)
+print("   *             *         "*2)
+print("  ******     ******        "*2)
+print("        *   *              "*2)
+print("        *   *              "*2)
+print("        *   *              "*2)
+print("        *   *              "*2)
+print("        *****              "*2)
